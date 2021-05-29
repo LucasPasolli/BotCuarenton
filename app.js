76e4eb50f555c00c98e31bf6dd5cbc7925488b01
unknown <LucasPasolli>
2021-05-29 15:34:31 -0300

    Implementacion de Embed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("Corriendo en terminal")
 
+const { timeStamp } = require('console');
 const Discord = require('discord.js');
 //const { userInfo } = require('os');
 //const newUsers = new Discord.Collection();
@@ -52,24 +53,43 @@ bot.on('message', message => {
         if (msg === prefix + 'commands' || msg === prefix + 'help' || msg === prefix + 'comandos') {
             setTimeout(() => message.delete(), 5000)
             message.channel.send(commandsList).then(msg => {
-                msg.delete({ timeout: 50000 /*time unitl delete in milliseconds*/});
+                msg.delete({ timeout: 50000});
             })
+
+            const embed = new MessageEmbed()
+                .setTitle('radiocheck')
+
         }
 
-        if (msg === prefix + 'ping') {
+        if (msg === prefix + 'torta') {
 
-            message.channel.send('Pong!')   
+            const embed = new Discord.MessageEmbed()
+                .setTitle('__**Torta Cuarentona**__')
+                .setDescription('Bueno, vamo a come')
+                .setColor("e3a144")
+                .addFields(
+                    {name: '__Vamos a necesitar:__', value: "-1 taza chica de aceite hasta la mitad. \n-150g azúcar. \n-2 huevos \n-1 banana pisada, mientras mas se parezca a una morcilla mejor \n-400g harina leudante \n-350ml leche "},
+                    {name: '__Segui estos pasos capo:__', value: "En un bowl vamos a poner el aceite, azúcar y los huevos y vamos a mezclar durante 2 minutos aproximadamente. \nLuego colocamos la harina alternando con la leche para poder mezclar facilmente. \nUna vez mezclado todo lo pones en un recipiente y lo mandas al horno al minimo durante unos 40-50 min."}
+                )
+                //.setImage("https://www.nicepng.com/png/full/433-4338735_cake-icon-cake-doodle-png.png")
+                .setThumbnail("https://jooinn.com/images/number-43-1.png")
+                .setTimestamp()
+                .setFooter("Compartile a la gorda, no seas forro", "https://www.nicepng.com/png/full/433-4338735_cake-icon-cake-doodle-png.png")
+                
+            message.author.send(embed);
         }
-        
+
         if (msg === prefix + 'info') {
-
-            message.channel.send(userInfo(sender));   
+            setTimeout(() => message.delete(), 5000)
+            message.channel.send(userInfo(sender)).then(msg => {
+                msg.delete({ timeout: 50000});
+            });   
         }
 
         if (msg === prefix + 'msgenv') {
             setTimeout(() => message.delete(), 5000)
             message.channel.send('Hola, como estas? Ví que me escribiste para saber el inutil dato de cuántos mensajes mandaste; btw mandaste: **' + userData[sender.id].messageSent + '**.').then(msg => {
-                msg.delete({ timeout: 50000 /*time unitl delete in milliseconds*/});
+                msg.delete({ timeout: 50000});
             })
         }
 
@@ -81,14 +101,14 @@ bot.on('message', message => {
             }
         }
         
-        if (msg === prefix + 'torta') {
-            message.author.send('__**Torta de La Gorda**__ \n-Una taza chica de aceite hasta la mitad. \n-150g azúcar \n-2 huevos \n-1 banana pisada, mientras mas se parezca a una morcilla mejor \n-400g harina leudante \n-350ml leche (este es mas a ojo, tipo yo le pongo dos vasos y hay veces que necesita menos o mas)')
-            //message.channel.send('Ahi te lo mande al privado monster')
-            setTimeout(() => message.delete(), 5000)
-            message.channel.send('Ahi te lo mande al privado REY').then(msg => {
-                    msg.delete({ timeout: 5000 /*time unitl delete in milliseconds*/});
-                })
-        }       
+        // if (msg === prefix + 'torta') {
+        //     message.author.send('__**Torta Cuarentona**__ \n-Una taza chica de aceite hasta la mitad. \n-150g azúcar \n-2 huevos \n-1 banana pisada, mientras mas se parezca a una morcilla mejor \n-400g harina leudante \n-350ml leche (este es mas a ojo, tipo yo le pongo dos vasos y hay veces que necesita menos o mas)')
+        //     //message.channel.send('Ahi te lo mande al privado monster')
+        //     setTimeout(() => message.delete(), 5000)
+        //     message.channel.send('Ahi te lo mande al privado REY').then(msg => {
+        //             msg.delete({ timeout: 5000});
+        //         })
+        // }       
     }
     
     if (msg.includes('verduras')) {
@@ -119,4 +139,23 @@ bot.on('guildMemberRemove', newMember => {
     welcomeChannel.send(usernameID + ' no se bancó la gira.');
 });
 
-bot.login('ODQ3MDk0ODc3NzE1MzAwNDIy.YK5ElQ.VuqwatDyuBU_qU975VjOErM11DE')
+bot.login('ODQ3MDk0ODc3NzE1MzAwNDIy.YK5ElQ.XvCaZFnBij20yeI4-A-HYcMxdzs')
+
+// message.channel.send({embed:{
+//     title: "__**Torta Cuarentona**__",
+//     description: "Mejor torta que vas a comer en tu vida!",
+//     color: "e3a144",
+//     fields:[
+//     {
+//         name: "Procedimiento",
+//         name: "Procedimiento",
+//         value: "**Descripcion,** se puede usar *formating* [y links](https://discord.com/developers/docs/resources/channel#channel-object)",
+//         inline:false,
+//     },
+// ],   
+//     timestamp: new Date(),
+//     footer: {
+//         text: "Buen dia",
+//         icon_url: 'https://cdn.icon-icons.com/icons2/2108/PNG/512/discord_icon_130958.png',
+//     }, 
+// }})
